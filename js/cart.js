5e8b7b9e48f72b1135f6931e11c8e3fabc1275a8
gmlesfrites <emilieviard.photographie@gmail.com>
2020-05-23 19:32:58 +0200

    W3C conforme sur tous les fichiers.html

--- js/cart.js
+++ js/cart.js
@@ -6,3 +6,34 @@
 
 // //suppression du panier
 // const removeFromCart = getElementsByClassName("removeFromCart");
+
+
+
+
+//Vérification du formulaire
+//nom, prénom, ville 
+function inputString() {
+    function surligne(champ, erreur) {
+        if (erreur)
+            champ.style.backgroundColor = "";
+        else
+            champ.style.backgroundColor = "";
+    }
+    console.log();
+}
+//téléphone
+function inputTel() {
+    console.log();
+}
+//code postal
+function inputPostalCode() {
+    console.log();
+}
+//adresse, commentaires
+function inputStrNum() {
+    console.log();
+}
+//email
+function inputMail() {
+    console.log();
+}

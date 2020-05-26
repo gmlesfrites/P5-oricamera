2c4f9475d14ecf8ff3a4c4a3b0ac90d778ec234b
gmlesfrites <emilieviard.photographie@gmail.com>
2020-05-26 07:01:41 +0200

    travail Design et contenu JS -- utilisation ${}

--- js/product.js
+++ js/product.js
@@ -5,6 +5,14 @@ const urlCam3 = 'http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b063';
 const urlCam4 = 'http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b064';
 const urlCam5 = 'http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b065';
 
+
+
+
+
+
+
+
+//fetch pour la récupération des données par id
 const fetchId = async function () {
     try {
         const response = await fetch(urlCam1);
@@ -19,25 +27,24 @@ const fetchId = async function () {
     }
 }
 
-//Utilisation des données récupérées pour les insérer dans la page
-fetchId().then(function (data) {
-    // Constitution Fiche produit
-    class Camera {
-        constructor(imageUrl, id, name, description, lenses, price) {
-            this.imageUrl = imageUrl;
-            this._id = id;
-            this.name = name;
-            this.description = description;
-            this.lenses = lenses;
-            this.price = price;
-        }
+// Constitution Fiche produit
+class Camera {
+    constructor(imageUrl, id, name, description, lenses, price) {
+        this.imageUrl = imageUrl;
+        this._id = id;
+        this.name = name;
+        this.description = description;
+        this.lenses = lenses;
+        this.price = price;
     }
+}
 
-    // Récup des données de chaque produit
-    const product = new Camera(data.imageUrl, data._id, data.name,
-        data.description, data.price);
-    console.log(product);
+//Utilisation des données de fetch pour les insérer dans la page
+fetchId().then(function (data) {
 
+    // // Récup des données de chaque produit
+    // const product = new Camera(data.imageUrl, data._id, data.name,
+    //     data.description, data.price);
 
     //Ajout de l'image
     const imageElt = document.getElementById("imageUrl");
@@ -46,34 +53,31 @@ fetchId().then(function (data) {
 
     //Ajout de la référence de produit
     const refElt = document.getElementById("ref");
-    refElt.insertAdjacentHTML('beforeend', "<p>" + data._id + "</p>");
-
+    refElt.innerHTML = `<p> <strong>Référence du produit : </strong>${data._id} </p>`;
+    console.log(data._id);
 
     //Nom du produit
     const singleTitleElt = document.getElementById("name");
-    singleTitleElt.innerHTML = `<h2>${data.name}</h2>`;
-
+    singleTitleElt.innerHTML = `<h2> ${data.name} </h2>`;
 
     //Description
     const descriptionElt = document.getElementById("description");
-    descriptionElt.insertAdjacentHTML('beforeend', "<p>" + data.description + "</p>");
-
+    descriptionElt.innerHTML = `<p><strong>Description : </strong> ${data.description} </p>`;
 
     // Optiques
-    const lensesElt = document.querySelector("ul");
+    const lensesElt = document.querySelector("select");
     const lensesData = data.lenses;
     for (let i = 0; i < lensesData.length; i++) {
-        let liElt = document.createElement("li");
-        liElt.innerHTML = lensesData[i];
-        console.log(liElt)
-        lensesElt.appendChild(liElt)
+        let optElt = document.createElement("option");
+        optElt.value = lensesData[i];
+        optElt.innerHTML = lensesData[i];
+        lensesElt.appendChild(optElt)
     }
-
 
     // Prix
     const singlePriceElt = document.getElementById("price");
     data.price = (data.price / 100).toFixed(2);
-    singlePriceElt.insertAdjacentHTML('beforeend', "<span>" + data.price + "&nbsp;" + "&#128" + "</span>");
+    singlePriceElt.innerHTML = `<p><strong>Prix : </strong> ${data.price} &#128; </p>`;
 });
 
 

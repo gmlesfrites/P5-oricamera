e7a26e2ce41f564154304318ce87eea0636278fc
gmlesfrites <emilieviard.photographie@gmail.com>
2020-06-20 08:53:11 +0200

    Essai fetch formulaire

--- js/form.js
+++ js/form.js
@@ -57,5 +57,29 @@ document.addEventListener('DOMContentLoaded', () => {
 const getInfos = event => {
     //interdire le rechargement pour éviter la perte d'infos
     event.preventDefault()
-    
+    //récupérer les données du formulaire
+    const contact = event.target;
+    let toSend = JSON.stringify(new FormData(contact));
+
+    //ajoute le panier
+    toSend.append(localStorage.getItem('products'));
 }
+
+//Envoi à l'API
+const url = 'http://localhost:3000/api/cameras/order';
+const headersFetch = new Headers();
+headersFetch.append('Content-type', 'application/json');
+
+const requestFetch = new Request(url, {
+    headers: headersFetch,
+    body: getInfos,
+    method: 'POST',
+});
+//console.log(req);
+fetch(requestFetch)
+    .then((Response) => Response.json())
+    .then((data) => {
+        console.log('Response from server');
+        console.log(data);
+    })
+    .catch(console.warn);

2866604071cd30398669128ee3b3f6c61f6d8ca5
gmlesfrites <emilieviard.photographie@gmail.com>
2020-06-27 08:44:57 +0200

    formatage du code

--- js/loader.js
+++ js/loader.js
@@ -3,13 +3,13 @@ const showContent = () => {
     let loader = document.querySelector(".container__loader");
     loader.setAttribute("style", "display: block");
     document.querySelector(".container__loader").classList.add('hidden');
-
 }
+
+//ne se joue qu'une fois par visite
 const hiddenLoader = () => {
     document.querySelector(".container__loader").setAttribute('style', "display:none");
 }
 const loader = sessionStorage.getItem('loader');
-
 if (!loader) {
     setTimeout(showContent, 3500)
     sessionStorage.setItem('loader', JSON.stringify('only one time'))
